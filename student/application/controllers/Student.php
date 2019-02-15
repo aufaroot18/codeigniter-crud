9288a36053a1e12fc2b1770277e992da1c2f9016
Aufa Billah <aufaroot18@gmail.com>
2019-02-15 15:02:11 +0700

    load model in controller Student

--- student/application/controllers/Student.php
+++ student/application/controllers/Student.php
@@ -1,9 +1,16 @@
 <?php 
 
 class Student extends CI_Controller {
+	public function __construct() {
+		parent::__construct();
+		$this->load->model('Student_model');
+	}
+
 	public function index() {
-		$this->load->view('admin/header');
-		$this->load->view('student/index');
+		$data['judul'] = 'Data Mahasiswa';
+		$data['mahasiswa'] = $this->Student_model->get_data_students();
+		$this->load->view('admin/header', $data);
+		$this->load->view('student/index', $data);
 		$this->load->view('admin/footer');
 	}
 }
