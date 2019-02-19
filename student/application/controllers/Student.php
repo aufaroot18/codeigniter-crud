d9d8b53b7fcce14e9b1207c4988e70f9319db8c7
Aufa Billah <aufaroot18@gmail.com>
2019-02-19 11:07:36 +0700

    Controller Student - Method Create

--- student/application/controllers/Student.php
+++ student/application/controllers/Student.php
@@ -4,6 +4,7 @@ class Student extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 		$this->load->model('Student_model');
+		$this->load->library('form_validation');
 	}
 
 	public function index() {
@@ -12,5 +13,21 @@ class Student extends CI_Controller {
 		$this->load->view('admin/header', $data);
 		$this->load->view('student/index', $data);
 		$this->load->view('admin/footer');
+	}
+
+	public function create() {
+		$this->form_validation->set_rules('nama', 'Nama', 'required');
+		$this->form_validation->set_rules('hp', 'No Hp', 'required|numeric');
+		$this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+		$this->form_validation->set_rules('alamat', 'Alamat', 'required');
+		if ($this->form_validation->run() == false) {
+			$this->load->view("admin/header");
+			$this->load->view("student/create");
+			$this->load->view("admin/footer");
+		}
+		else {
+			$this->Student_model->create_student();
+			redirect('student');
+		}
 	}
 }
