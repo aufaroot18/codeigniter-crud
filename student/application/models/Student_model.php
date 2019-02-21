6f5aaa866ef16cdee65756f30acd20ceb7c74bec
Aufa Billah <aufaroot18@gmail.com>
2019-02-21 11:09:23 +0700

    Model Student - method create_student

--- student/application/models/Student_model.php
+++ student/application/models/Student_model.php
@@ -5,4 +5,16 @@ class Student_model extends CI_Model {
 		$query = $this->db->get('mahasiswa');
 		return $query->result();
 	}
+
+	public function create_student() {
+		$data = [
+			"nama" => $this->input->post('nama'),
+			"jurusan" => $this->input->post('jurusan'),
+			"no_hp" => $this->input->post('hp'),
+			"email" => $this->input->post('email'),
+			"alamat" => $this->input->post('alamat'),
+		];
+
+		$this->db->insert('mahasiswa', $data);
+	}
 }
