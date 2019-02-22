a4ca59cf0d2aa7c301886851a3825fbb97a8d29b
Aufa Billah <aufaroot18@gmail.com>
2019-02-22 11:52:33 +0700

    Controller Student - method update

--- student/application/controllers/Student.php
+++ student/application/controllers/Student.php
@@ -16,12 +16,13 @@ class Student extends CI_Controller {
 	}
 
 	public function create() {
+		$data['judul'] = 'Tambah Data Mahasiswa';
 		$this->form_validation->set_rules('nama', 'Nama', 'required');
 		$this->form_validation->set_rules('hp', 'No Hp', 'required|numeric');
 		$this->form_validation->set_rules('email', 'Email', 'required|valid_email');
 		$this->form_validation->set_rules('alamat', 'Alamat', 'required');
 		if ($this->form_validation->run() == false) {
-			$this->load->view("admin/header");
+			$this->load->view("admin/header", $data);
 			$this->load->view("student/create");
 			$this->load->view("admin/footer");
 		}
@@ -29,5 +30,25 @@ class Student extends CI_Controller {
 			$this->Student_model->create_student();
 			redirect('student');
 		}
+	}
+
+	public function update($id) {
+		$this->form_validation->set_rules('nama', 'Nama', 'required');
+		$this->form_validation->set_rules('hp', 'No Hp', 'required|numeric');
+		$this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+		$this->form_validation->set_rules('alamat', 'Alamat', 'required');
+
+		if ($this->form_validation->run() == false) {
+			$data['judul'] = 'Edit Data Mahasiswa';
+			$data["mahasiswa"] = $this->Student_model->get_one_student($id);
+			$this->load->view("admin/header", $data);
+			$this->load->view("student/update", $data);
+			$this->load->view("admin/footer");
+		}
+		else {
+			$this->Student_model->update_student();
+			redirect("student");
+		}
+		
 	}
 }
