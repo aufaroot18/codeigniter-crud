6c71008cb818ba8c41db23bf086712833c0ea05c
Aufa Billah <aufaroot18@gmail.com>
2019-02-28 11:52:13 +0700

    Controller Login - Method Login

--- student/application/controllers/Login.php
+++ student/application/controllers/Login.php
@@ -1,7 +1,21 @@
 <?php 
 
 class Login extends CI_Controller {
+	public function __construct() {
+		parent::__construct();
+		$this->load->model('Login_model');
+	}
 	public function index() {
 		return $this->load->view('login/index');
+	}
+
+	public function login() {
+		$hasil = $this->Login_model->login();
+		if ($hasil == 0) {
+			redirect('login');
+		}
+		else {
+			redirect('admin');
+		}
 	}
 }
