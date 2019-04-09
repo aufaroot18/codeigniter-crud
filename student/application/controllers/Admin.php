e5ff6d9129785b208122faf35742e1343acb38bf
Aufa Billah <aufaroot18@gmail.com>
2019-04-10 06:01:33 +0700

    check session in admin

--- student/application/controllers/Admin.php
+++ student/application/controllers/Admin.php
@@ -2,8 +2,15 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
+	public function __construct() {
+		parent::__construct();
+		if (!$this->session->has_userdata('logged_in')) {
+			redirect('login');
+		}
+	}
 	public function index() {
-		$this->load->view('admin/header');
+		$data['judul'] = 'Halaman Admin';
+		$this->load->view('admin/header', $data);
 		$this->load->view('admin/index');
 		$this->load->view('admin/footer');
 	}
