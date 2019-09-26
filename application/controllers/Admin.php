64c849d0a8d6e2dee87065d2aa12e20deafb3459
dianynf <dianbugas@gmail.com>
2019-09-26 16:05:57 +0700

    nambahin dikit

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,17 +1,20 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Admin extends CI_Controller {
-	public function __construct() {
+class Admin extends CI_Controller
+{
+	public function __construct()
+	{
 		parent::__construct();
 		if (!$this->session->has_userdata('logged_in')) {
 			redirect('login');
 		}
 	}
-	public function index() {
+	public function index()
+	{
 		$data['judul'] = 'Halaman Admin';
 		$this->load->view('admin/header', $data);
 		$this->load->view('admin/index');
-		$this->load->view('admin/footer');
+		$this->load->view('admin/footer', $data);
 	}
 }
