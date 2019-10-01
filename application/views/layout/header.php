c1522335ec1fb5f5af0a315b0600e1d6a1a08e7b
dianynf <dianbugas@gmail.com>
2019-10-01 13:26:41 +0700

    kontribusi hacktoberfest

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?= $judul ?></title>
   <!-- Tell the browser to be responsive to screen width -->
-  <!-- masih error -->
+  <!-- masih errors -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
   <link rel="stylesheet" href="<?= base_url(); ?>assets/AdminLTE/bower_components/bootstrap/dist/css/bootstrap.min.css">
